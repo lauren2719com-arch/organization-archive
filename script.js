936afb489acb10e4e3058fb48d2461bf393f9a7c
lauren2719com-arch <lauren2719.com@gmail.com>
2026-06-27 18:49:10 +0330

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ document.querySelector("button").addEventListener("click", function () {
         alert("Searching for: " + search);
     }
 });
+const overlay = document.getElementById("overlay");
+const modalImage = document.getElementById("modalImage");
+const close = document.getElementById("close");
+
+function showImage(file){
+    modalImage.src = "images/" + file;
+    overlay.style.display = "flex";
+}
+
+close.onclick = () => {
+    overlay.style.display = "none";
+};
+
+overlay.onclick = (e)=>{
+    if(e.target===overlay){
+        overlay.style.display="none";
+    }
+};
