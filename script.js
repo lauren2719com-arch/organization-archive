415462d5a98c469470ef39f3266cf7398e57af3c
lauren2719com-arch <lauren2719.com@gmail.com>
2026-06-27 16:41:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Organization Scripts
+document.querySelector("button").addEventListener("click", function () {
+    const search = document.querySelector("input").value;
+
+    if (search.trim() === "") {
+        alert("Please enter a file name.");
+    } else {
+        alert("Searching for: " + search);
+    }
+});
